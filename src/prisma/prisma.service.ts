04ded9996a76796b4032ad6b8193f35737cce02a
Gustavo-Meroni <gustavopmeroni@gmail.com>
2026-07-29 12:38:11 -0300

    fix(prisma): adjust dbPath

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,22 +1,23 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
+import * as path from 'path';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    // Agora passamos apenas a "Config" diretamente para o adaptador!
-    // Ele mesmo vai se encarregar de gerenciar o @libsql/client
+    // Apontando direto para a raiz do projeto (onde o arquivo real com as tabelas está!)
+    const dbPath = path.join(process.cwd(), 'dev.db');
+
     const adapter = new PrismaLibSql({
-      url: 'file:./prisma/dev.db',
+      url: `file:${dbPath}`,
     });
 
-    // Ligamos o motor do Prisma
     super({ adapter });
   }
 
   async onModuleInit() {
     await this.$connect();
-    console.log('📦 Banco de dados conectado com sucesso!');
+    console.log(`📦 Banco de dados conectado com sucesso em: ${path.join(process.cwd(), 'dev.db')}`);
   }
 }
