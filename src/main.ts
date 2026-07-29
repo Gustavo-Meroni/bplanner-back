61dd1dc572dd7b03f996faf7964ef8b037ed4b38
Gustavo-Meroni <gustavopmeroni@gmail.com>
2026-07-29 12:40:50 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // <-- Importamos o Swagger
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 1. Criamos o documento de configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('BPlanner API')
+    .setDescription('API para gerenciamento de finanças pessoais do BPlanner')
+    .setVersion('1.0')
+    .build();
+
+  // 2. Geramos o documento baseado no nosso app
+  const document = SwaggerModule.createDocument(app, config);
+
+  // 3. Montamos a página do Swagger na rota '/api'
+  SwaggerModule.setup('api', app, document);
+
+  // Ativamos o CORS (isso será essencial quando seu Front-end for consumir a API!)
+  app.enableCors();
+
+  await app.listen(3000);
 }
 bootstrap();
